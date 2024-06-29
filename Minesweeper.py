ddc99d237db07de810e9e2fc097b966604669cc3
Ian Sanchez Baca <iansanchez066@gmail.com>
2024-06-28 20:22:08 -0700

    created function that initializes a grid with 0's and then randomly places bombs by marking them with a 1

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,2 +1,59 @@
+# @author: Ian Sanchez Baca
+#
+#
 
-print("hello world")
+import pygame
+import random # used to randomly place bombs
+
+print("Initializing pygame.")
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+running = False
+EASY = [10, 10] # [the size of the grid, the number of bombs]
+
+
+def initBombGrid(size, bombs):
+    # creating a size x size bomb grid starting with 0
+    ## if there is no bomb the tile will be 1
+    ## if there is a bomb the tile will be 0
+    bombGrid = [[0 for _ in range(size)] for _ in range(size)]
+
+    # creating bombs
+    bombsPlaced = 0
+
+    while bombsPlaced < bombs:
+        x = random.randint(0, size-1)
+        y = random.randint(0, size-1)
+
+        if bombGrid[x][y] == 0:
+            bombGrid[x][y] = 1
+            bombsPlaced += 1
+
+    print("\nPrinting bombGrid!")
+    for i in range(size):
+        print(bombGrid[i])
+
+    return bombGrid
+
+
+initBombGrid(EASY[0], EASY[1])
+
+
+while running:
+    # leave this like this in case the user closes
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # user input
+
+    # update
+
+    # render
+
+print("bye bye!")
+pygame.quit()
+
+
+
+# def newGame():
--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Minesweeper in python
 
+## Introduction... i guess.
 I am using pygame because I was too lazy to set up SDL library for c++.
 
 Thats it. Explanation over.
+
+## Documentation
+
+### Gameplay
+
+#### EASY
+* 10x10 board
+* 10 bombs randomly spread out
+* 10 flags.
+
+#### MEDIUM 
+* 18x18 board 
+* 40 bombs
+* 40 flags
+
