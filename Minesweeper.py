d2adcf053362c2ab3b07df27a0b6928f7b0a9212
Ian Sanchez Baca <iansanchez066@gmail.com>
2024-06-28 20:39:17 -0700

    i understand what CreateNumGrid is doing but need to figure out how its doing it

--- Minesweeper.py
+++ Minesweeper.py
@@ -35,8 +35,27 @@ def initBombGrid(size, bombs):
 
     return bombGrid
 
+def create_number_grid(bomb_grid):
+    size = len(bomb_grid)
+    number_grid = [[0 for _ in range(size)] for _ in range(size)]
+    
+    for i in range(size):
+        for j in range(size):
+            if bomb_grid[i][j] == 1:
+                for x in range(max(0, i-1), min(size, i+2)):
+                    for y in range(max(0, j-1), min(size, j+2)):
+                        if (x, y) != (i, j):
+                            number_grid[x][y] += 1
+    
+    print("\nPrinting numGrid!")
+    for i in range(size):
+        print(number_grid[i])
+    # return number_grid  
 
-initBombGrid(EASY[0], EASY[1])
+
+temp = initBombGrid(EASY[0], EASY[1])
+
+create_number_grid(temp)
 
 
 while running:
